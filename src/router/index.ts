5e75b2fb5966e03410071e0b83774c1ab2b72d37
pydick <p1053192517@163.com>
2020-04-15 13:41:15 +0800

    quchu

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,12 @@ import VueRouter, { RouteConfig } from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes: Array<RouteConfig> = []
+const routes: Array<RouteConfig> = [
+  {
+    path: "/login",
+    component: () => import("@/views/login/index.vue"),
+  },
+]
 
 const router = new VueRouter({
   mode: 'history',
