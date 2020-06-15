17bad3e72eb79ef2c39cd80a9f61e138572771b9
pydick <p1053192517@163.com>
2020-06-15 13:42:30 +0800

    修改ip,优化

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,8 @@
-export let ip!: string
-export let port!: string | number
+const environment = process.env.NODE_ENV; // production else development
 
-if (process.env.NODE_ENV === "production") {
-  ip = location.hostname // IP地址
-  port = location.port // 端口号
-} else if (process.env.NODE_ENV === "development") {
-  ip = "10.138.146.62" // IP地址 何天宝10.138.146.44 彭勇10.138.146.62 伟伟 10.138.146.59 锋训 10.138.146.52
-  port = 80 // 端口号
-}
-export const baseApi = `http://${ip}:${port}` // configs for production and development
+export const ip = environment === "production" ? location.hostname : "10.138.146.62";
+export const port = environment === "production" ? location.port : "80"
+export const baseApi = `http://${ip}:${port}`;
 // 有tab切换的页面,存储选中页面的名称
 export enum Sessions {
   userName = "user_name", // 用户名
